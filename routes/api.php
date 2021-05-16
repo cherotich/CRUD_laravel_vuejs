f9b530c5e1eb65aae5eac8947b30cba04a20a210
cherotich <carolcaro60@gmail.com>
2021-05-16 07:55:46 +0300

    Created an api to save contacts

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Save contacts
+Route::post('save_contact','contactController@save_contact');
+//get contacts
+Route::get('getContacts','contactController@getContacts');
